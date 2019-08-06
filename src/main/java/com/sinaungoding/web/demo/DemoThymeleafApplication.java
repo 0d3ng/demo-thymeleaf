7f8c086a0a95e4712e84345a36fd4c0a630f3f94
od3ng <noprianto@polinema.ac.id>
2019-08-06 13:49:40 +0700

    implementasi paging

--- src/main/java/com/sinaungoding/web/demo/DemoThymeleafApplication.java
+++ src/main/java/com/sinaungoding/web/demo/DemoThymeleafApplication.java
@@ -2,12 +2,19 @@ package com.sinaungoding.web.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 public class DemoThymeleafApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoThymeleafApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoThymeleafApplication.class, args);
+    }
+
+    @Bean
+    public SpringDataDialect springDataDialect() {
+        return new SpringDataDialect();
+    }
 
 }
