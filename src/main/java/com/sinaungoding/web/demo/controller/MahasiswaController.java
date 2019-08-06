5291abe79c1e0393ce0ccf1ce4b72559711c7edc
od3ng <noprianto@polinema.ac.id>
2019-08-06 12:53:53 +0700

    sudah bisa tampil list all mahasiswa

--- src/main/java/com/sinaungoding/web/demo/controller/MahasiswaController.java
+++ src/main/java/com/sinaungoding/web/demo/controller/MahasiswaController.java
@@ -4,18 +4,22 @@ import com.sinaungoding.web.demo.dao.MahasiswaDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/mahasiswa")
 public class MahasiswaController {
     @Autowired
     private MahasiswaDao mahasiswaDao;
 
-    @GetMapping("/mahasiswas")
+    @GetMapping("/index")
     public ModelMap getAll(Pageable pageable) {
         return new ModelMap().addAttribute("mahasiswas", mahasiswaDao.findAll(pageable));
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/index";
     }
 }
